642877e287602ed681a0917e3f9cab20516cf15e
Mark <markpnoi26@gmail.com>
2020-08-24 23:26:33 -0700

    fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/webhook', (req, res) => {
             let webhook_event = entry.messaging[0]
             console.log(webhook_event)
         })
-        console.log(fetchByOneKey)
+        fetchOneByKey()
         res.status(200).send('EVENT_RECEIVED')
     } else {
         res.sendStatus(404)
