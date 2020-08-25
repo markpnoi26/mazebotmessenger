032c7e5bd0fa61c93457fa54b54a4b940105d0a5
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-25 15:37:33 -0700

    edited simple message response

--- botResponse.js
+++ botResponse.js
@@ -1,6 +1,20 @@
-// Handles messages events
-const handleMessage = (sender_psid, received_message) => {
+const request = require('request');
 
+// Handles messages events
+const handleMessage = (sender_psid, received_message, userInfo) => {
+    let response;
+    
+    // Check if the message contains text
+    if (received_message.text) {    
+
+        // Create the payload for a basic text message
+        response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response); 
 }
 
 // Handles messaging_postbacks events
@@ -10,7 +24,25 @@ const handlePostback = (sender_psid, received_postback) => {
 
 // Sends response messages via the Send API
 const callSendAPI = (sender_psid, response) => {
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
 
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.page_access_token },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    }); 
 }
 
 
