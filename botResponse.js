40c9e8798e3e3a868d3e575acc353ff1077a68df
Mark <markpnoi26@gmail.com>
2020-08-25 23:31:23 -0700

    added another greet message

--- botResponse.js
+++ botResponse.js
@@ -8,41 +8,21 @@ const handleGreetings = (sender_psid, received_message) => {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Welcome to MazeBot!",
-                    "subtitle": `
-                        Mazebot is aa small coding challenge maze solver. Our mazebot generates a random maze based on difficulty and it is your job to solve this maze, by coding a solution into the messenger.
-
-                        How Does it work?
-                        A sample maze: * looks like this*
-
-                        The solution: <d, r, r, d> or < d, loop(r, 2), d >
-
-                        Code structure:
-                        directions: u < up >, d < down >, l < left >, r < right >, loop(<direction>, <repeat number>)
-
-                        If the code has errors, the bot will notify you.
-                        if your code hits a wall, or does not reach the target, a visual representation will be sent to you.
-
-                        Rules:
-                        - A single operation is separated by comma.
-                        - Each direction (u,l,r,d), and loop(<dir>, <repeat>) is a single operation.
-                        - The code must not hit any walls in order to succeed.
-                        - The maze will have exactly one solution.
-                        `,
+                    "title": "Select Your Maze Difficulty",
                         "buttons": [
                         {
                             "type": "postback",
-                            "title": "Easy",
+                            "title": "Easy (5x5 maze)",
                             "payload": "easy",
                         },
                         {
                             "type": "postback",
-                            "title": "Medium",
+                            "title": "Medium (7x7 maze)",
                             "payload": "med",
                         },
                         {
                             "type": "postback",
-                            "title": "Hard",
+                            "title": "Hard (9x11 maze)",
                             "payload": "hard",
                         }
                     ],
@@ -51,6 +31,30 @@ const handleGreetings = (sender_psid, received_message) => {
         }
     }
 
+    callSendAPI(sender_psid, { 
+        'text': `
+            Welcome to MazeBot - a small coding challenge maze solver. Our mazebot generates a random maze based on difficulty and it is your job to solve this maze, by coding a solution into the messenger.
+
+            How Does it work?
+            A sample maze: *looks like this*
+
+            The solution: <d, r, r, d> or <d, loop(r, 2), d>
+
+            Code structure: 
+            directions: u <up>, d <down>, l <left>, r <right>, loop(<direction>, <repeat number>)
+
+            If the code has errors, the bot will notify you. 
+            if your code hits a wall, or does not reach the target, a visual representation will be sent to you.
+
+            Rules:
+            - A single operation is separated by comma.
+            - Each direction (u,l,r,d), and loop(<dir>, <repeat>) is a single operation.
+            - The code must not hit any walls in order to succeed.
+            - The maze will have exactly one solution.
+
+            5x5 (easy), 7x7 (medium), 7x11 (hard)
+        ` 
+    })
     callSendAPI(sender_psid, response)
 }
 
