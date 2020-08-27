bb05cb38a197e5a2ad72812a81fdbddaea956c78
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-27 14:52:36 -0700

    limited number of buttons

--- botResponse.js
+++ botResponse.js
@@ -6,64 +6,30 @@ const sendInitialGreetings = (sender_psid) => {
     }
 
     const responsePostback = {
-        // "attachment": {
-        //     "type": "template",
-        //     "payload": {
-        //         "template_type": "generic",
-        //         "elements": [{
-        //             "title": "Select Your Maze Difficulty",
-        //             "buttons": [
-        //                 {
-        //                     "type": "postback",
-        //                     "title": "Easy (5x5 maze)",
-        //                     "payload": "easy",
-        //                 },
-        //                 {
-        //                     "type": "postback",
-        //                     "title": "Medium (7x7 maze)",
-        //                     "payload": "medium",
-        //                 },
-        //                 {
-        //                     "type": "postback",
-        //                     "title": "Hard (9x9 maze)",
-        //                     "payload": "hard",
-        //                 },
-        //                 {
-        //                     "type": "postback",
-        //                     "title": "Hard (7x11 maze)",
-        //                     "payload": "hardLong",
-        //                 }
-        //             ],
-        //         }]
-        //     }
-        // }
-        "attachment":{
-            "type":"template",
-            "payload":{
-              "template_type":"generic",
-              "elements":[
-                 {
-                  "title":"Welcome!",
-                  "image_url":"https://petersfancybrownhats.com/company_image.png",
-                  "subtitle":"We have the right hat for everyone.",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com/view?item=103",
-                    "webview_height_ratio": "tall",
-                  },
-                  "buttons":[
-                    {
-                      "type":"web_url",
-                      "url":"https://petersfancybrownhats.com",
-                      "title":"View Website"
-                    },{
-                      "type":"postback",
-                      "title":"Start Chatting",
-                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                    }              
-                  ]      
-                }
-              ]
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Select Your Maze Difficulty",
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Easy (5x5 maze)",
+                            "payload": "easy",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Medium (7x7 maze)",
+                            "payload": "medium",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Hard (9x9 maze)",
+                            "payload": "hard",
+                        }
+                    ],
+                }]
             }
         }
     }
