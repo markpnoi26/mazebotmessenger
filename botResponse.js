57fedc9268e6640a2d4074297a3147cb5f1b5d3e
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-08-27 14:49:13 -0700

    testing initial greeting

--- botResponse.js
+++ botResponse.js
@@ -6,35 +6,64 @@ const sendInitialGreetings = (sender_psid) => {
     }
 
     const responsePostback = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Select Your Maze Difficulty",
-                    "buttons": [
-                        {
-                            "type": "postback",
-                            "title": "Easy (5x5 maze)",
-                            "payload": "easy",
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Medium (7x7 maze)",
-                            "payload": "medium",
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Hard (9x9 maze)",
-                            "payload": "hard",
-                        },
-                        {
-                            "type": "postback",
-                            "title": "Hard (7x11 maze)",
-                            "payload": "hardLong",
-                        }
-                    ],
-                }]
+        // "attachment": {
+        //     "type": "template",
+        //     "payload": {
+        //         "template_type": "generic",
+        //         "elements": [{
+        //             "title": "Select Your Maze Difficulty",
+        //             "buttons": [
+        //                 {
+        //                     "type": "postback",
+        //                     "title": "Easy (5x5 maze)",
+        //                     "payload": "easy",
+        //                 },
+        //                 {
+        //                     "type": "postback",
+        //                     "title": "Medium (7x7 maze)",
+        //                     "payload": "medium",
+        //                 },
+        //                 {
+        //                     "type": "postback",
+        //                     "title": "Hard (9x9 maze)",
+        //                     "payload": "hard",
+        //                 },
+        //                 {
+        //                     "type": "postback",
+        //                     "title": "Hard (7x11 maze)",
+        //                     "payload": "hardLong",
+        //                 }
+        //             ],
+        //         }]
+        //     }
+        // }
+        "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "elements":[
+                 {
+                  "title":"Welcome!",
+                  "image_url":"https://petersfancybrownhats.com/company_image.png",
+                  "subtitle":"We have the right hat for everyone.",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://petersfancybrownhats.com/view?item=103",
+                    "webview_height_ratio": "tall",
+                  },
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://petersfancybrownhats.com",
+                      "title":"View Website"
+                    },{
+                      "type":"postback",
+                      "title":"Start Chatting",
+                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                    }              
+                  ]      
+                }
+              ]
             }
         }
     }
@@ -162,6 +191,7 @@ const callSendAPI = (sender_psid, response) => {
         }, (err, res, body) => {
             if (!err) {
                 console.log('message sent!')
+                console.log(response)
                 resolve("success")
             } else {
                 console.error("Unable to send message:" + err);
